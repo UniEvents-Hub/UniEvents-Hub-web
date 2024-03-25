09d40d2dec663da1aa10a6172897f13a270724db
alif2499 <alifjcc99@gmail.com>
2024-03-25 15:17:35 -0600

    sign up and sign in page design

--- src/app/Layout/AuthLayout.tsx
+++ src/app/Layout/AuthLayout.tsx
@@ -3,8 +3,9 @@
 
 import { usePathname } from "next/navigation";
 import React, { useEffect, useState } from "react";
-import SignUp from '../Auth/Signup';
+import Signup from "../Auth/Signup";
 import Login from '../Auth/Login';
+import UserInterest from "../Auth/UserInterest";
 
 export default function AuthLayout({
     children,
@@ -12,6 +13,7 @@ export default function AuthLayout({
     children: React.ReactNode;
 }) {
     const [user, setUser] = useState(null);
+    const [isSignup, setIsSignup] = useState(false)
 
     useEffect(() => { }, []);
 
@@ -19,18 +21,23 @@ export default function AuthLayout({
     useEffect(() => {
     }, []);
 
+    const handleSignup = (enableSignup: boolean) => {
+        setIsSignup(enableSignup)
+
+    }
+
     if ((user)) return <>{children}</>;
     else
         return (
             <div className="flex flex-col">
 
                 <div>
-                    <img
+                    {/* <img
                         className="h-[44px] w-[100px] mt-100 ml-20"
                         src="/images/Ctribe-logo.svg"
                         alt="c-tribe"
-                    />
-                    {!user ? <SignUp /> : <Login />}
+                    /> */}
+                    {isSignup ? <Signup /> : <Login handleSignup={handleSignup}/>}
                 </div>
             </div>
         );
